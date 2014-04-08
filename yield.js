ea275f8cc018c0feaa65c99d604667962eda89fc
shinexavier <shine.xavier@gmail.com>
2014-04-08 13:19:20 +0530

    Update yield.js
    With Local Function Context

--- yield.js
+++ yield.js
@@ -1,27 +1,92 @@
-Array.prototype.iterator = function (fn) {
+Array.prototype.getIterator = function () {
     "use strict";
-    var that = {};
+    var that = {},
+		curry = function (fn) {
+			var slice = Array.prototype.slice;
+			return function () {
+				return fn.apply(null, slice.apply(arguments));
+			};
+		},
+		getFuncCtxt = function (fnIndex, input) {
+			var funcCtxt = that.fnContexts[fnIndex];
+			if (funcCtxt === undefined) {
+				funcCtxt = {
+					"input" : input,
+					"outputSequence" : []
+				};
+				that.fnContexts[fnIndex] = funcCtxt;
+			} else {
+				funcCtxt.input = input;
+			}
+			return funcCtxt;
+		},
+		isYieldEmpty = function () {
+			return ((that.yieldIndex + 1) === that.inputSequence.length);
+		},
+		yieldX = function () {
+			var args = arguments,
+				resultX = null;
+			function core() {
+				var i,
+					result = null,
+					fn = null,
+					fnCtxt = null,
+					evalSkipped = false,
+					slice = Array.prototype.slice;
+				that.yieldIndex += 1;
+				result = that.inputSequence[that.yieldIndex];
+				if (args.length > 0) {
+					for (i = 0; i < args.length; i += 1) {
+						fn = args[i];
+						fnCtxt = getFuncCtxt(i, result);
+						result = fn.call(fnCtxt);
+						if (result === null) {
+							evalSkipped = true;
+							break;
+						} else {
+							fnCtxt.outputSequence.push(result);
+						}
+					}
+					if (result !== null) {
+						that.outputSequence.push(result);
+					}
+					return result;
+				} else {
+					return that.inputSequence[that.yieldIndex];
+				}
+			}
+			while ((resultX === null) && (!that.isYieldEmpty())) {
+				resultX = core();
+			}
+			return resultX;
+		};
+    that.fnContexts = [];
     that.inputSequence = this;
     that.length = that.inputSequence.length;
     that.outputSequence = [];
     that.yieldIndex = -1;
-    that.isYieldEmpty = function () {
-        return ((that.yieldIndex + 1) === that.inputSequence.length);
-    };
-    that.yieldX = function () {
-        that.yieldIndex += 1;
-        return fn(that.inputSequence[that.yieldIndex]);
-    };
+    that.isYieldEmpty = isYieldEmpty;
+    that.yieldX = yieldX;
     return that;
 };
-function print(x) {
-    "use strict";
-    console.log(x * x);
-}
 
 //Test Harness
+function unique() {
+    "use strict";
+    if (this.outputSequence.indexOf(this.input) < 0) {
+        return this.input;
+    } else {
+        return null;
+    }
+}
+
+function square() {
+    "use strict";
+    return (this.input * this.input);
+}
+
 var x = [2, 3, 4, 5, 2, 3, 4, 1];
-var continuation = x.iterator(print);
+var continuation = x.getIterator();
 while (!continuation.isYieldEmpty()) {
-continuation.yieldX();
+    console.log(continuation.yieldX(unique,square,square));
 }
