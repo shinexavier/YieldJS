ec4fc6935743292572ae537906018e065abe664e
shinexavier <shine.xavier@gmail.com>
2014-04-08 21:17:15 +0530

    Changed the iterator interface
    1. Added "moveNext" method & "current" property to:
    a. Make the behaviour consistent with the standard Iterator interface with C# (my personal bias)
    b. Fix an issue w.r.t getting a trailing null element in case of invalid output towards the end of collection (fixed by deferring the evaluation to moveNext method as opposed to the method/property that fetches the current element in collection)

--- yield.js
+++ yield.js
@@ -1,72 +1,67 @@
 Array.prototype.getIterator = function () {
     "use strict";
     var that = {},
-		curry = function (fn) {
-			var slice = Array.prototype.slice;
-			return function () {
-				return fn.apply(null, slice.apply(arguments));
-			};
-		},
-		getFuncCtxt = function (fnIndex, input) {
-			var funcCtxt = that.fnContexts[fnIndex];
-			if (funcCtxt === undefined) {
-				funcCtxt = {
-					"input" : input,
-					"outputSequence" : []
-				};
-				that.fnContexts[fnIndex] = funcCtxt;
-			} else {
-				funcCtxt.input = input;
-			}
-			return funcCtxt;
-		},
-		isYieldEmpty = function () {
-			return ((that.yieldIndex + 1) === that.inputSequence.length);
-		},
-		yieldX = function () {
-			var args = arguments,
-				resultX = null;
-			function core() {
-				var i,
-					result = null,
-					fn = null,
-					fnCtxt = null,
-					evalSkipped = false,
-					slice = Array.prototype.slice;
-				that.yieldIndex += 1;
-				result = that.inputSequence[that.yieldIndex];
-				if (args.length > 0) {
-					for (i = 0; i < args.length; i += 1) {
-						fn = args[i];
-						fnCtxt = getFuncCtxt(i, result);
-						result = fn.call(fnCtxt);
-						if (result === null) {
-							evalSkipped = true;
-							break;
-						} else {
-							fnCtxt.outputSequence.push(result);
-						}
-					}
-					if (result !== null) {
-						that.outputSequence.push(result);
-					}
-					return result;
-				} else {
-					return that.inputSequence[that.yieldIndex];
+    getFuncCtxt = function (fnIndex, input) {
+        var funcCtxt = that.fnContexts[fnIndex];
+        if (funcCtxt === undefined) {
+            funcCtxt = {
+                "input" : input,
+                "outputSequence" : []
+            };
+            that.fnContexts[fnIndex] = funcCtxt;
+        } else {
+            funcCtxt.input = input;
+        }
+        return funcCtxt;
+    },
+    isYieldEmpty = function () {
+        return ((that.yieldIndex + 1) === that.inputSequence.length);
+    },
+	current = null,
+    moveNext = function () {
+        var args = arguments,
+        yieldedResult = null,
+        core = function () {
+            var i,
+            result = null,
+            fn = null,
+            fnCtxt = null;
+            that.yieldIndex += 1;
+            result = that.inputSequence[that.yieldIndex];
+            if (args.length > 0) {
+                for (i = 0; i < args.length; i += 1) {
+                    fn = args[i];
+                    fnCtxt = getFuncCtxt(i, result);
+                    result = fn.call(fnCtxt);
+                    if (result === null) {
+                        break;
+                    } else {
+                        fnCtxt.outputSequence.push(result);
+                    }
+                }
+				if (result !== null) {
+					that.outputSequence.push(result);
+					that.current = result;
 				}
-			}
-			while ((resultX === null) && (!that.isYieldEmpty())) {
-				resultX = core();
-			}
-			return resultX;
-		};
+				return result;
+            } else {
+                that.current = that.inputSequence[that.yieldIndex];
+				that.outputSequence.push(that.current);
+				return that.current;
+            }
+        };
+        while ((yieldedResult === null) && (!isYieldEmpty())) {
+            yieldedResult = core();
+        }
+        return (yieldedResult !== null) ? true : false;
+    };
     that.fnContexts = [];
     that.inputSequence = this;
     that.length = that.inputSequence.length;
     that.outputSequence = [];
     that.yieldIndex = -1;
-    that.isYieldEmpty = isYieldEmpty;
-    that.yieldX = yieldX;
+    that.current = null;
+    that.moveNext = moveNext;
     return that;
 };
 
@@ -85,8 +80,25 @@ function square() {
     return (this.input * this.input);
 }
 
-var x = [2, 3, 4, 5, 2, 3, 4, 1];
-var continuation = x.getIterator();
-while (!continuation.isYieldEmpty()) {
-    console.log(continuation.yieldX(unique,square,square));
+function filter() {
+	"use strict";
+	return (this.input % 2 === 0) ? this.input : null; 
 }
+
+function skip1() {
+	"use strict";
+	if (this.hasOwnProperty('count')) {
+		this.count += 1;
+	} else {
+		this.count = 0;
+	}
+	return (this.count % 2 !== 0) ? this.input : null; 
+}
+
+var x = [1,2,3,200,null,1,2,3,200],
+	continuation = x.getIterator();
+
+while (continuation.moveNext(unique,skip1)) {
+    console.log(continuation.current);
+}
+console.log(continuation.outputSequence);
