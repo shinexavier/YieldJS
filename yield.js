6f86f28affe7310f83dd9e4497143e3260430cba
shinexavier <shine.xavier@gmail.com>
2014-04-23 19:59:04 +0530

    Added Generator Code Block

--- yield.js
+++ yield.js
@@ -46,47 +46,149 @@ Array.prototype.getIterator = function () {
 		},
 		moveNext = function () {
 			var args = arguments,
-				yieldedResult = null,
-				core = function () {
-					var i,
-						result = null,
-						fn = null,
-						fnCtxt = null,
-						returnVal = null;
-					yieldIndex += 1;
-					result = inList[yieldIndex];
-					if (args.length > 0) {
-						for (i = 0; i < args.length; i += 1) {
-							fn = args[i];
-							fnCtxt = getFunctionContext(i, result);
-							result = fn.call(null, fnCtxt);
-							if (result === null) {
-								break;
-							} else {
-								fnCtxt.outList.push(result);
-							}
+			yieldedResult = null,
+			core = function () {
+				var i,
+				result = null,
+				fn = null,
+				fnCtxt = null,
+				returnVal = null;
+				yieldIndex += 1;
+				result = inList[yieldIndex];
+				if (args.length > 0) {
+					for (i = 0; i < args.length; i += 1) {
+						fn = args[i];
+						fnCtxt = getFunctionContext(i, result);
+						result = fn.call(null, fnCtxt);
+						if (result === null) {
+							break;
+						} else {
+							fnCtxt.outList.push(result);
 						}
-						if (result !== null) {
-							it.outList.push(result);
-							it.current = result;
-						}
-						returnVal = result;
-					} else {
-						it.current = inList[yieldIndex];
-						it.outList.push(it.current);
-						returnVal = it.current;
 					}
-					return returnVal;
-				};
+					if (result !== null) {
+						it.outList.push(result);
+						it.current = result;
+					}
+					returnVal = result;
+				} else {
+					it.current = inList[yieldIndex];
+					it.outList.push(it.current);
+					returnVal = it.current;
+				}
+				return returnVal;
+			};
 			while ((yieldedResult === null) && (!isYieldEmpty())) {
 				//Recursive call to find the next non-null value
 				yieldedResult = core();
 			}
 			return (yieldedResult !== null) ? true : false;
+		},
+		iterate = function () {
+			var slice = Array.prototype.slice;
+			while (moveNext.apply(null, slice.apply(arguments))) {
+				//Force chained evaluation of continuation methods (if any).
+			}
+			return it.outList;
+		},
+		reset = function () {
+			fnContexts.length = 0;
+			fnContexts = [];
+			yieldIndex = -1;
+			it.outList.length = 0;
+			it.outList = [];
+			it.current = null;
 		};
     it.length = inList.length;
     it.outList = [];
     it.current = null;
     it.moveNext = moveNext;
+    it.iterate = iterate;
+    it.reset = reset;
     return it;
+};
+
+Function.prototype.getGenerator = function (setCount1) {
+    "use strict";
+    var fnGen = this,
+		numberOfElements = setCount1,
+		slice = Array.prototype.slice,
+		gen = {},
+		fnContexts = [],
+		yieldIndex = -1,
+		getFunctionContext = function (fnIndex, input) {
+			var fnContext = fnContexts[fnIndex];
+			if (fnContext === undefined) {
+				fnContext = {
+					"index" : 0,
+					"current" : input,
+					"outList" : []
+				};
+				fnContexts[fnIndex] = fnContext;
+			} else {
+				fnContext.current = input;
+				fnContext.index += 1;
+			}
+			return fnContext;
+		},
+		isYieldEmpty = function () {
+			return ((yieldIndex + 1) === numberOfElements);
+		},
+		moveNext = function () {
+			var args = arguments,
+			yieldedResult = null,
+			core = function () {
+				var i,
+				result = null,
+				fn = null,
+				fnCtxt = null;
+				yieldIndex += 1;
+				result = fnGen.apply(null, []);
+				if (args.length > 0) {
+					for (i = 0; i < args.length; i += 1) {
+						fn = args[i];
+						fnCtxt = getFunctionContext(i, result);
+						result = fn.call(null, fnCtxt);
+						if (result === null) {
+							break;
+						} else {
+							fnCtxt.outList.push(result);
+						}
+					}
+					if (result !== null) {
+						gen.current = result;
+					}
+				} else {
+					gen.current = result;
+				}
+				return result;
+			};
+			while ((yieldedResult === null) && (!isYieldEmpty())) {
+				//Recursive call to find the next non-null value
+				yieldedResult = core();
+			}
+			return (yieldedResult !== null) ? true : false;
+		},
+		generate = function () {
+			var outList = [];
+			//Force generation in one iteration.
+			while (moveNext.apply(null, slice.apply(arguments))) {
+				outList.push(gen.current);
+			}
+			return outList;
+		},
+		nextSet = function (setCount2) {
+			if (arguments.length > 0) {
+				numberOfElements = setCount2;
+			}
+			fnContexts.length = 0;
+			fnContexts = [];
+			yieldIndex = -1;
+			gen.current = null;
+		};
+    gen.current = null;
+    gen.moveNext = moveNext;
+    gen.generate = generate;
+    gen.nextSet = nextSet;
+    return gen;
 };
